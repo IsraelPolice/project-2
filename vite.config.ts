a603c197e54ad6fdb9480cc5fb342440b928d6a6
IsraelPolice <dvirbareket4@gmail.com>
2025-11-09 16:46:50 +0200

    chore: set Vite base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // חשוב: הנתיב בריפו שלך
+  // חשוב ל-GitHub Pages:
   base: "/project-2/",
   server: {
     port: 5173,
